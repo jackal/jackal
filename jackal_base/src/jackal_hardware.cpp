cf7842d09d6a9e4752611337aba8dbdb2bdcb971
Administrator <administrator@testbox.testbox>
2017-06-20 14:26:52 -0400

    Fixed roslinter error of { on same line as for loop

--- jackal_base/src/jackal_hardware.cpp
+++ jackal_base/src/jackal_hardware.cpp
@@ -42,7 +42,8 @@ JackalHardware::JackalHardware()
   ros::V_string joint_names = boost::assign::list_of("front_left_wheel")
       ("front_right_wheel")("rear_left_wheel")("rear_right_wheel");
 
-  for (unsigned int i = 0; i < joint_names.size(); i++) {
+  for (unsigned int i = 0; i < joint_names.size(); i++)
+  {
     hardware_interface::JointStateHandle joint_state_handle(joint_names[i],
         &joints_[i].position, &joints_[i].velocity, &joints_[i].effort);
     joint_state_interface_.registerHandle(joint_state_handle);
