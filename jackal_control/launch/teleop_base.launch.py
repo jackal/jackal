10c4035c219f9ddf13e6c5ea6f5780cfe8a420b4
Roni Kreinin <rkreinin@clearpathrobotics.com>
2022-06-23 13:24:43 -0400

    ROS2 jackal_control

---
+++ jackal_control/launch/teleop_base.launch.py
@@ -0,0 +1,36 @@
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    filepath_config_twist_mux = PathJoinSubstitution(
+        [FindPackageShare('jackal_control'), 'config', 'twist_mux.yaml']
+    )
+
+    filepath_config_interactive_markers = PathJoinSubstitution(
+        [FindPackageShare('jackal_control'), 'config', 'teleop_interactive_markers.yaml']
+    )
+
+    node_interactive_marker_twist_server = Node(
+        package='interactive_marker_twist_server',
+        executable='marker_server',
+        name='twist_server_node',
+        remappings={('cmd_vel', 'twist_marker_server/cmd_vel')},
+        parameters=[filepath_config_interactive_markers],
+        output='screen',
+    )
+
+    node_twist_mux = Node(
+        package='twist_mux',
+        executable='twist_mux',
+        output='screen',
+        remappings={('/cmd_vel_out', '/jackal_velocity_controller/cmd_vel_unstamped')},
+        parameters=[filepath_config_twist_mux]
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(node_interactive_marker_twist_server)
+    ld.add_action(node_twist_mux)
+    return ld
